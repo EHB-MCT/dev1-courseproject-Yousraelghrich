24fdefb185b84267b296d4c77c8b258c2e616449
Yousraelghrich <yousra.el.ghrich@student.ehb.be>
2024-10-09 17:23:58 +0200

    black background done

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,18 @@
 "use strict";
 
-alert("Draw your space invader here");
+//alert("Draw your space invader here");
+
+backGround();
+
+function backGround() {
+	let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let context = canvas.getContext("2d");
+
+	context.beginPath();
+	context.fillRect(50, 50, 300, 300);
+	context.fillStyle = "black";
+	context.fill();
+	context.stroke();
+}
